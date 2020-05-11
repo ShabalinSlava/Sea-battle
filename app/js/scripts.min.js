bc788a82445bf9139e6c564fdc8e9ef3c2d183cf
Shabalin Slava <svaznoy0309@gmail.com>
2020-05-11 18:46:57 +0800

    random ship creation

--- app/js/scripts.min.js
+++ app/js/scripts.min.js
@@ -8,25 +8,79 @@ document.addEventListener('DOMContentLoaded', function(){
   const header = document.querySelector('.header');
 
   const game = {
-    ships: [
-      {
-        location: ['26', '36', '46', '56'],
-        hit: ['', '', '', '',]
-      },
-      {
-        location: ['11', '12', '13'],
-        hit: ['', '', '']
-      },
-      {
-        location: ['69', '79'],
-        hit: ['', '']
-      },
-      {
-        location: ['32'],
-        hit: ['']
-      },
-    ],
-    shipCount: 4,
+    ships: [],
+    shipCount: 0,
+    optionShip: {
+      count: [1, 2, 3, 4],
+      size:[4, 3, 2, 1],
+    },
+    collision: new Set(),
+    generateShip() {
+      for (let i = 0; i < this.optionShip.count.length; i++) {
+        for (let j = 0; j < this.optionShip.count[i]; j++) {
+          const size = this.optionShip.size[i];
+          const ship = this.generateOptionsShip(size);
+          this.ships.push(ship);
+          this.shipCount++;
+        }
+      }
+    },
+    generateOptionsShip(shipSize) {
+      const ship = {
+        hit: [],
+        location: []
+      };
+      // Задаем координаты
+      const direction = Math.random() < 0.5;
+      let x, y;
+      if (direction) {   // Если true, то карабль будет горизонтально
+        x = Math.floor(Math.random() * 10);
+        y = Math.floor(Math.random() * (10 - shipSize));
+
+      } else { // Если false, то карабль будет вертикально
+        x = Math.floor(Math.random() * (10 - shipSize));
+        y = Math.floor(Math.random() * 10);
+      }
+
+      // Сложение координат
+      for (let i = 0; i < shipSize; i++) {
+        if (direction) {
+          ship.location.push(x + '' + (y + i))
+        } else {
+          ship.location.push((x + i) + '' + y)
+        }
+        ship.hit.push('');
+      };
+      if (this.checkCollision(ship.location)){
+        return this.generateOptionsShip(shipSize)
+      };
+      this.addCollision(ship.location);
+
+      return ship;
+
+    },
+    // Что бы корабли не накладывались друг на друга
+    checkCollision(location) {
+      for (const coord of location) {
+        if (this.collision.has(coord)) {
+          return true
+        }
+      }
+    },
+    addCollision(location) {
+      for (let i = 0; i < location.length; i++) {
+        const startCoordX = location[i][0] -1;
+        for (let j = startCoordX; j < startCoordX + 3; j++) {
+          const startCoordY = location[i][2] -1;
+          for (let z = startCoordY; z < startCoordY + 3; z++ ) {
+            if (j >= 0 && j < 10 && z >= 0 && z < 10) {
+              const coord = j + '' + z;
+                this.collision.add(coord)
+            }
+          }
+        }
+      }
+    }
   }
 
   const play = {
@@ -63,47 +117,56 @@ document.addEventListener('DOMContentLoaded', function(){
 
   const fire = (event) => {
     const target = event.target;
-    if (target.classList.length !== 0 || target.tagName !== 'TD') return;
-    show.miss(target);
-    play.updateData = 'shot';
+    if (target.classList.length !== 0 ||
+      target.tagName !== 'TD' ||
+      !game.shipCount) return;
+      show.miss(target);
+      play.updateData = 'shot';
 
-    for (let i = 0; i < game.ships.length; i++) {
-      const ship = game.ships[i];
-      const index = ship.location.indexOf(target.id);
-      if (index >= 0) {
-        show.hit(target);
-        play.updateData = 'hit';
-        ship.hit[index] = 'x';
-        const life = ship.hit.indexOf('');
-        if (life < 0) {
-          play.updateData = 'dead';
-          for (const id of ship.location) {
-            show.dead(document.getElementById(id));
-          }
+      for (let i = 0; i < game.ships.length; i++) {
+        const ship = game.ships[i];
+        const index = ship.location.indexOf(target.id);
+        if (index >= 0) {
+          show.hit(target);
+          play.updateData = 'hit';
+          ship.hit[index] = 'x';
+          const life = ship.hit.indexOf('');
+          if (life < 0) {
+            play.updateData = 'dead';
+            for (const id of ship.location) {
+              show.dead(document.getElementById(id));
+            }
 
-          game.shipCount -=1;
-          if (game.shipCount < 1) {
-            header.textContent = 'Игра окончена!';
-            header.style.color = 'red';
+            game.shipCount -=1;
+            if (!game.shipCount) {
+              header.textContent = 'Игра окончена!';
+              header.style.color = 'red';
 
-            if (play.shot < play.record || play.record === 0) {
-              localStorage.setItem('seaBattleRecord', play.shot);
-              play.record = play.shot;
-              play.render();
+              if (play.shot < play.record || play.record === 0) {
+                localStorage.setItem('seaBattleRecord', play.shot);
+                play.record = play.shot;
+                play.render();
+              }
             }
           }
         }
       }
-    }
-  };
+    };
 
-  const init = () => {
-    enemy.addEventListener('click', fire);
-    play.render();
+    // Инициализация
+    const init = () => {
+      enemy.addEventListener('click', fire); // Клик по клетке
+      play.render();
+      game.generateShip();
+      again.addEventListener('click', () => {  // Клик по кнопке начать заново
+        location.reload();
+      });
 
-    again.addEventListener('click', () => {
-      location.reload();
-    });
-  };
-  init();
-});
+      record.addEventListener('dblclick', () => {  //Двойной клик по рекорду и он сбрасывается
+        localStorage.clear();
+        play.record = 0;
+        play.render();
+      });
+    };
+    init();
+  });
